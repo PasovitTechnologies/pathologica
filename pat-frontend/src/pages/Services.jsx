bb500fa0373e93aee1746c4f3f79dc9c383478f9
Gaurav Pandey <gauravpndy00@gmail.com>
2025-04-07 17:57:59 +0530

    Improved responsiveness

--- pat-frontend/src/pages/Services.jsx
+++ pat-frontend/src/pages/Services.jsx
@@ -158,13 +158,13 @@ export const services = [
 const Services = () => {
   return (
     <section
-      className="-z-10 relative pt-6 pb-8 md:pb-13 bg-cover bg-center bg-fixed"
+      className="-z-10 relative pt-6 pb-8 lg:pb-13 bg-cover bg-center bg-fixed"
       style={{
         backgroundImage: `url('https://static.wixstatic.com/media/a31a5a_e99bbbf837484c6eb2a4ff285508b4db~mv2.jpg')`,
       }}
     >
       <div className="absolute inset-0 bg-gray-200/70"></div>
-      <div className="relative max-w-4xl mx-auto px-4 md:px-0">
+      <div className="relative max-w-4xl mx-auto px-4 lg:px-0">
         <h2 className="text-2xl md:text-[2.85rem] tracking-tight font-bold text-center mb-2">
           Стоимость услуг
         </h2>
@@ -176,17 +176,17 @@ const Services = () => {
             <h3 className="text-xl md:text-2xl text-center font-semibold mb-6 md:mb-9">
               {service.category}
             </h3>
-            <div className="space-y-4 md:space-y-0">
+            <div className="space-y-4 lg:space-y-0">
               {service.items.map((item, idx) => (
                 <div
                   key={idx}
-                  className="border shadow-md bg-white shadow-black/40 flex flex-col md:flex-row md:items-end"
+                  className="border shadow-md bg-white shadow-black/40 flex flex-col lg:flex-row lg:items-end"
                 >
                   <div
-                    className="w-full md:w-[82%] border-b md:border-b-0 md:border-r px-4 py-6 md:px-8 md:py-12"
+                    className="w-full lg:w-[82%] border-b md:border-b-0 lg:border-r px-4 py-6 md:px-8 md:py-12"
                     style={{
                       boxShadow:
-                        "inset 0 -10px 6px -10px rgba(0, 0, 0, 0.3) md:inset -10px 0 6px -10px rgba(0, 0, 0, 0.5)",
+                        "inset 0 -10px 6px -10px rgba(0, 0, 0, 0.3) lg:inset -10px 0 6px -10px rgba(0, 0, 0, 0.5)",
                     }}
                   >
                     <h4 className="text-base md:text-lg font-semibold">
@@ -195,7 +195,7 @@ const Services = () => {
                     <p className="text-gray-600 italic text-sm md:text-base">
                       {item.description}
                     </p>
-                    <p className="text-lg font-bold mt-2 md:hidden">
+                    <p className="text-lg font-bold mt-2 lg:hidden">
                       {item.price}
                     </p>
                     {item.extra && (
@@ -203,7 +203,7 @@ const Services = () => {
                         {item.extra.map((extraItem, i) => (
                           <li key={i} className="flex justify-between">
                             <span>{extraItem.name}</span>
-                            <span className="font-semibold md:hidden">
+                            <span className="font-semibold lg:hidden">
                               {extraItem.price}
                             </span>
                           </li>
@@ -211,7 +211,7 @@ const Services = () => {
                       </ul>
                     )}
                   </div>
-                  <div className="hidden w-full md:block md:w-[18%] px-3 md:px-5 py-4 md:py-10 text-right">
+                  <div className="hidden w-full lg:block lg:w-[18%] px-3 md:px-5 py-4 md:py-10 text-right">
                     <p className="text-base md:text-lg mt-2 md:mt-4">
                       {item.price}
                     </p>
